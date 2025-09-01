9b82ed34185572ccba7b9536b38e546c43eb2853
Abdul-Lenda <a.aliyu@lendastack.io>
2025-09-01 13:23:34 +0100

    feat: implemented search for users.

---
+++ UserManagement/ApplicationFeatures/Users/Queries/SearchUserQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.ApplicationFeatures.Users.Queries
+{
+    public class SearchUserQuery : IRequest<List<User>>
+    {
+        public string KeyWord { get; set; }
+        public int PageSize { get; set; }
+        public int Page { get; set; }
+
+        public SearchUserQuery(string keyWord, int pageSize, int page)
+        {
+            KeyWord = keyWord;
+            PageSize = pageSize;
+            Page = page;
+        }
+    }
+}
